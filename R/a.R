fab347bbfa9aea232e9bff43dd19651e192ab8a2
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-07-10 21:23:43 +0200

    better

--- R/a.R
+++ R/a.R
@@ -4,21 +4,19 @@ myfun <- function(
   some_param = "default_value"
 )
 {
-  #' Do some of my stuff
-  #'
+  #' @title Do some of my stuff
   initialize()
   check_argument(some_param)
 
-  #' Gets my stuff done. Snappy.
-  #'
+  #' @description Gets my stuff done. Snappy.
   do_stuff()
 
-  #' The \code{\link{obtw}} function makes sure time constraints are kept.
+  #' @details The \code{\link{obtw}} function makes sure time constraints are kept.
   obtw(do_it_quickly = TRUE)
 }
 
 
 #' @export
 obtw <- function(...) {
-  #' Oh, by the way
+  #' @title Oh, by the way
 }
